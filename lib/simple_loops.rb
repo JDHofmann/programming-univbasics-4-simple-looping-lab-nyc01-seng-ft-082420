2e20a3c8ebbb58966e6ed2a949ff6745c84e1181
JD Brewer Hofmann <justindhofmann@gmail.com>
2020-08-11 21:22:35 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,37 @@
-# Write your methods here
+def loop_message_five_times(string)
+  counter = 0
+  while counter < 6 do
+  puts string
+  counter += 1
+  end
+end 
+
+def loop_message_n_times(string, num)
+  counter = 0
+  while counter < num do
+  puts string
+  counter += 1
+  end
+end 
+
+def output_array(array)
+  counter = 0
+while array[counter] do
+  puts array[counter]
+  counter += 1
+end
+end
+
+def return_string_array(array)
+  new_array = []
+  counter = 0 
+  while counter <= array.length do
+    new_array.push(array[counter])
+    counter += 1 
+  end 
+  counter_2 = 0 
+  while counter_2 <= new_array.length do 
+    array[counter_2].to_s 
+  end 
+new_array
+end
